3c8aa8b6b90f1ed931fbf5ee275cb2ea6c628e9b
alexramosiii-dev <alexramosiii.dev@gmail.com>
2023-10-25 10:24:35 +0800

    Added UI for the Rock Paper Scissor Game

--- rps/app.js
+++ rps/app.js
@@ -1,12 +1,15 @@
 function getComputerChoice() {
     let pick = ["rock", "paper", "scissor"];
     let selection = pick[Math.floor(Math.random() * 3)];
+    
+    document.querySelector("#computer-box").style.backgroundImage = `url(./images/${selection}.png)`
+    
     return selection;
 }
 
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection) {
-        return "Tie";
+        return "tie";
     }else if (playerSelection == "rock") {
         return (computerSelection == "scissor") ? "player" : "computer";
     }else if (playerSelection == "paper") {
@@ -18,41 +21,52 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    let playerSelection;
-    let computerSelection;
-    let result;
+function playGame(){
+    let announcer = document.querySelector("#announcer");
+    
+    if (round < 4){
+        let playerSelection = document.querySelector("#player-box").dataset.weapon;
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
 
-    let playerScore = 0;
-    let computerScore = 0;
-
-    let round = 0;
-    while (round < 5) {
-        playerSelection = prompt().toLowerCase();
-        computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
-
-        if(result === "player") {
-            playerScore++;
-            console.log("---Player Wins This Round!---");
+        switch (result){
+        case "player":
+            announcer.textContent = `${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+            playerScore.textContent++;
+            round++;
+            break;
+        case "computer":
+            announcer.textContent = `${playerSelection.toUpperCase()} is beaten by ${computerSelection.toUpperCase()}`
+            computerScore.textContent++;
+            round++;
+            break;
+        case "tie":
+            announcer.textContent = "Same weapon. No point for both!"
+            break;
+        default:
+            announcer.textContent = "Invalid weapon"
+            break;
         }
-        else if(result == "computer") {
-            computerScore++;
-            console.log("---Computer Wins This Round!---");
-        }
-        else {
-            console.log("---This Round Is Not Valid! No points for Each---");
-            continue;
-        }
-
-        console.log(`Player Score: ${playerScore}`)
-        console.log(`Computer Score: ${computerScore}`)
-        round++;
+        if(round > 3) announcer.textContent = `Game has ended ${(playerScore.textContent>computerScore.textContent)?"Player":"Computer"} won the game.`
     }
-
-    console.log((computerScore > playerScore) ? `\n\n\n===COMPUTER WINS! ${computerScore} : ${playerScore}===`:`===PLAYER WINS! ${playerScore} : ${computerScore}===`);
 }
 
+//change player box when a weapon is selected
+let weapons = document.querySelectorAll(".container-weapon > button");
+weapons.forEach(weapon => weapon.addEventListener("click",
+    (e) => {
+        let box = document.querySelector("#player-box");
+        box.style.backgroundImage = getComputedStyle(e.target).backgroundImage;
+        box.dataset.weapon = e.target.dataset.weapon
+    }
+))
 
-game();
+let playbtn = document.querySelector("#btn-play");
+playbtn.addEventListener("click", playGame);
+
+let playerScore = document.querySelector("#score-text-player");
+let computerScore = document.querySelector("#score-text-computer");
+let round = 1;
+
+document.querySelector("#btn-reset").addEventListener("click", e => location.reload())
 
